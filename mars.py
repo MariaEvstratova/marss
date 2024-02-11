01c9a4aa59ac87cadb2f9e8b73aa70169226e5fd
mariaevstratova <evstratova.mashenka@gmail.com>
2024-02-11 21:15:40 +0700

    Initial commit

---
+++ mars.py
@@ -0,0 +1,38 @@
+from flask import Flask, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/promotion_image')
+def return_promotion_image():
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+                    <title>Колонизация</title>
+                  </head>
+                  <body>
+                    <h1>Жди нас, Марс!</h1>
+                    <img src="{url_for('static', filename='img/mars.png')}" alt="здесь должна была быть картинка, но не нашлась">
+                    <div class="alert alert-secondary" role="alert">
+                      Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                      И начнем с Марса!
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      Присоединяйся!
+                    </div>
+                  </body>
+                </html>"""
+
+if __name__ == '__main__':
+    app.run(port=8000, host='127.0.0.1')
